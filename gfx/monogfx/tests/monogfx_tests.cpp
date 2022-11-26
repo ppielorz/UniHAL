603ac80e3ee0b58a69e0edb7086c0b0a4ddc8c84
Paweł Pielorz <pawel.pielorz@gmail.com>
2022-11-26 13:48:06 +0000

    Refactored tests

--- gfx/monogfx/tests/monogfx_tests.cpp
+++ gfx/monogfx/tests/monogfx_tests.cpp
@@ -74,141 +74,132 @@ TEST_GROUP(monoGFX_setPixel)
 
 TEST(monoGFX_setPixel, nullPointer)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
-
-    monoGFX_status_t status = monoGFX_setPixel(NULL, 0, 0);
-
-    LONGS_EQUAL(monoGFX_status_nullPointer, status);
+    LONGS_EQUAL(monoGFX_status_nullPointer, monoGFX_setPixel(NULL, 0, 0));
 }
 
 TEST(monoGFX_setPixel, xAxisExceeded)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
-
-    monoGFX_status_t status = monoGFX_setPixel(&gfx, 32, 0);
-
-    LONGS_EQUAL(monoGFX_status_xAxisExceeded, status);
+    LONGS_EQUAL(monoGFX_status_xAxisExceeded, monoGFX_setPixel(&gfx, 32, 0));
 }
 
 TEST(monoGFX_setPixel, yAxisExceeded)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
-
-    monoGFX_status_t status = monoGFX_setPixel(&gfx, 0, 16);
-
-    LONGS_EQUAL(monoGFX_status_yAxisExceeded, status);
+    LONGS_EQUAL(monoGFX_status_yAxisExceeded, monoGFX_setPixel(&gfx, 0, 16));
 }
 
 TEST(monoGFX_setPixel, plainSetPixel)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 0));
     expectedGfxBuffer[0] |= 0x80;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 0));
     expectedGfxBuffer[0] |= 0x40;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 1));
     expectedGfxBuffer[4] |= 0x80;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 1));
     expectedGfxBuffer[4] |= 0x40;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 }
 
 TEST(monoGFX_setPixel, reversedBitOrder)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     gfx.bitReverseOrder = true;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 0));
     expectedGfxBuffer[0] |= 0x01;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 0));
     expectedGfxBuffer[0] |= 0x02;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 1));
     expectedGfxBuffer[4] |= 0x01;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 1));
     expectedGfxBuffer[4] |= 0x02;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 }
 
 TEST(monoGFX_setPixel, rotateClockwise)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_clockwise);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_clockwise));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 0));
     expectedGfxBuffer[1] |= 0x01;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 0));
     expectedGfxBuffer[3] |= 0x01;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 1));
     expectedGfxBuffer[1] |= 0x02;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 1));
     expectedGfxBuffer[3] |= 0x02;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 }
 
 TEST(monoGFX_setPixel, rotateCounterclockwise)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_counterclockwise);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_counterclockwise));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 0));
     expectedGfxBuffer[62] |= 0x80;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 0));
     expectedGfxBuffer[60] |= 0x80;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 1));
     expectedGfxBuffer[62] |= 0x40;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 1));
     expectedGfxBuffer[60] |= 0x40;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 }
 
 TEST(monoGFX_setPixel, rotateHalfTurn)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_halfTurn);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_halfTurn));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 0));
     expectedGfxBuffer[63] |= 0x01;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 0));
     expectedGfxBuffer[63] |= 0x02;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 1));
     expectedGfxBuffer[59] |= 0x01;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 1, 1);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 1, 1));
     expectedGfxBuffer[59] |= 0x02;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 }
@@ -240,59 +231,50 @@ TEST_GROUP(monoGFX_clearPixel)
 
 TEST(monoGFX_clearPixel, nullPointer)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
-
-    monoGFX_status_t status = monoGFX_clearPixel(NULL, 0, 0);
-
-    LONGS_EQUAL(monoGFX_status_nullPointer, status);
+    LONGS_EQUAL(monoGFX_status_nullPointer, monoGFX_clearPixel(NULL, 0, 0));
 }
 
 TEST(monoGFX_clearPixel, xAxisExceeded)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
-
-    monoGFX_status_t status = monoGFX_clearPixel(&gfx, 32, 0);
-
-    LONGS_EQUAL(monoGFX_status_xAxisExceeded, status);
+    LONGS_EQUAL(monoGFX_status_xAxisExceeded, monoGFX_clearPixel(&gfx, 32, 0));
 }
 
 TEST(monoGFX_clearPixel, yAxisExceeded)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
-
-    monoGFX_status_t status = monoGFX_clearPixel(&gfx, 0, 16);
-
-    LONGS_EQUAL(monoGFX_status_yAxisExceeded, status);
+    LONGS_EQUAL(monoGFX_status_yAxisExceeded, monoGFX_clearPixel(&gfx, 0, 16));
 }
 
 TEST(monoGFX_clearPixel, plainClearPixel)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 0));
     expectedGfxBuffer[0] |= 0x80;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_clearPixel(&gfx, 0, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_clearPixel(&gfx, 0, 0));
     expectedGfxBuffer[0] &= ~0x80;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 }
 
 TEST(monoGFX_clearPixel, reversedBitOrder)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
     gfx.bitReverseOrder = true;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_setPixel(&gfx, 0, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_setPixel(&gfx, 0, 0));
     expectedGfxBuffer[0] |= 0x01;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
-    monoGFX_clearPixel(&gfx, 0, 0);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_clearPixel(&gfx, 0, 0));
     expectedGfxBuffer[0] &= ~0x01;
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 }
@@ -303,7 +285,6 @@ TEST(monoGFX_clearPixel, reversedBitOrder)
 
 TEST_GROUP(monoGFX_getPixel)
 {
-    monoGFX_status_t status = monoGFX_status_success;
     monoGFX_t gfx = {0};
     uint8_t gfxBuffer[64] = {0};
     bool pixelSet = false;
@@ -324,31 +305,21 @@ TEST_GROUP(monoGFX_getPixel)
 
 TEST(monoGFX_getPixel, nullPointer)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
-
-    monoGFX_status_t status = monoGFX_getPixel(NULL, 0, 0, &pixelSet);
-    LONGS_EQUAL(monoGFX_status_nullPointer, status);
-
-    status = monoGFX_getPixel(&gfx, 0, 0, NULL);
-    LONGS_EQUAL(monoGFX_status_nullPointer, status);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
+    LONGS_EQUAL(monoGFX_status_nullPointer, monoGFX_getPixel(NULL, 0, 0, &pixelSet));
+    LONGS_EQUAL(monoGFX_status_nullPointer, monoGFX_getPixel(&gfx, 0, 0, NULL));
 }
 
 TEST(monoGFX_getPixel, xAxisExceeded)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
-
-    monoGFX_status_t status = monoGFX_getPixel(&gfx, 32, 0, &pixelSet);
-
-    LONGS_EQUAL(monoGFX_status_xAxisExceeded, status);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
+    LONGS_EQUAL(monoGFX_status_xAxisExceeded, monoGFX_getPixel(&gfx, 32, 0, &pixelSet));
 }
 
 TEST(monoGFX_getPixel, yAxisExceeded)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
-
-    monoGFX_status_t status = monoGFX_getPixel(&gfx, 0, 16, &pixelSet);
-
-    LONGS_EQUAL(monoGFX_status_yAxisExceeded, status);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none));
+    LONGS_EQUAL(monoGFX_status_yAxisExceeded, monoGFX_getPixel(&gfx, 0, 16, &pixelSet));
 }
 
 TEST(monoGFX_getPixel, plainGetPixel)
@@ -377,23 +348,23 @@ TEST(monoGFX_getPixel, plainGetPixel)
 #include <time.h>
 TEST(monoGFX_setPixel, aaa)
 {
-    monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_halfTurn);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 32, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_halfTurn);
     MEMCMP_EQUAL(expectedGfxBuffer, gfxBuffer, sizeof(expectedGfxBuffer));
 
     clock_t startTime = clock();
 
     for(size_t i = 0U; i < 1000000; i++)
     {
-        monoGFX_setPixel(&gfx, 0, 0);
-        monoGFX_setPixel(&gfx, 1, 0);
-        monoGFX_setPixel(&gfx, 2, 0);
-        monoGFX_setPixel(&gfx, 3, 0);
-        monoGFX_setPixel(&gfx, 4, 0);
-        monoGFX_setPixel(&gfx, 5, 0);
-        monoGFX_setPixel(&gfx, 6, 0);
-        monoGFX_setPixel(&gfx, 7, 0);
-        monoGFX_setPixel(&gfx, 8, 0);
-        monoGFX_setPixel(&gfx, 9, 0);
+        monoGFX_setPixel(&gfx, 0, 0));
+        monoGFX_setPixel(&gfx, 1, 0));
+        monoGFX_setPixel(&gfx, 2, 0));
+        monoGFX_setPixel(&gfx, 3, 0));
+        monoGFX_setPixel(&gfx, 4, 0));
+        monoGFX_setPixel(&gfx, 5, 0));
+        monoGFX_setPixel(&gfx, 6, 0));
+        monoGFX_setPixel(&gfx, 7, 0));
+        monoGFX_setPixel(&gfx, 8, 0));
+        monoGFX_setPixel(&gfx, 9, 0));
     }
 
     clock_t endTime = clock();
@@ -426,24 +397,24 @@ TEST(monoGFX_invalidation, init)
 {
     monoGFX_t gfx = {0};
     uint8_t gfxBuffer[32] = {0};
-    monoGFX_init(&gfx, 16, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
+    LONGS_EQUAL(monoGFX_status_success, monoGFX_init(&gfx, 16, 16, gfxBuffer, sizeof(gfxBuffer), monoGFX_rotation_none);
     //printAscii(&gfx);
     //printf("%02X %02X %02X %02X %02X %02X %02X %02X\n", gfxBuffer[0], gfxBuffer[1], gfxBuffer[2], gfxBuffer[3], gfxBuffer[4], gfxBuffer[5], gfxBuffer[6], gfxBuffer[7]);
     for(size_t i = 0U; i < 16 * 16; i++)
     {
         monoGFX_setPixel(&gfx, i % 16, i / 16);
         printAscii(&gfx);
-        sleep(1);
+        sleep(1));
 
     }
-    //monoGFX_setPixel(&gfx, 7, 0);
+    //monoGFX_setPixel(&gfx, 7, 0));
     extern const GFXfont FreeSans9pt7b;
     //monoGFX_print(&gfx, 0, 0, &FreeSans9pt7b, "test");
     //printf("%02X %02X %02X %02X %02X %02X %02X %02X\n", gfxBuffer[0], gfxBuffer[1], gfxBuffer[2], gfxBuffer[3], gfxBuffer[4], gfxBuffer[5], gfxBuffer[6], gfxBuffer[7]);
-    //monoGFX_setPixel(&gfx, 1, 0);
+    //monoGFX_setPixel(&gfx, 1, 0));
     //printAscii(&gfx);
     //printf("%02X %02X %02X %02X %02X %02X %02X %02X\n", gfxBuffer[0], gfxBuffer[1], gfxBuffer[2], gfxBuffer[3], gfxBuffer[4], gfxBuffer[5], gfxBuffer[6], gfxBuffer[7]);
-    //monoGFX_setPixel(&gfx, 0, 1);
+    //monoGFX_setPixel(&gfx, 0, 1));
     //printAscii(&gfx);
     //printf("%02X %02X %02X %02X %02X %02X %02X %02X\n", gfxBuffer[0], gfxBuffer[1], gfxBuffer[2], gfxBuffer[3], gfxBuffer[4], gfxBuffer[5], gfxBuffer[6], gfxBuffer[7]);
 }
